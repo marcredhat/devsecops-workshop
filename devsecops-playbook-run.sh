f9b4f526d55a1c54a2ed18f22800022f60f87d51
Dan Clark <danielmclark@gmail.com>
2019-11-05 12:10:40 +0000

    Source the contents of aws.env to set the AWS API credentials instead of having to manually export them

--- devsecops-playbook-run.sh
+++ devsecops-playbook-run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Source the environment files with the AWS access keys
+if test -f vars/aws.env
+then
+  . vars/aws.env
+fi
+
 : ${AWS_ACCESS_KEY_ID?"Need to set AWS_ACCESS_KEY_ID"}
 : ${AWS_SECRET_ACCESS_KEY?"Need to set AWS_SECRET_ACCESS_KEY"}
 
